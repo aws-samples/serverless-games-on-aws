aaa2f4d5ee5bcd6dbab4fc1c753c3b8faaa2feb0
Gena Gizzi <50339505+gizzigg@users.noreply.github.com>
2019-08-28 15:52:31 -0700

    Update Cognito.cs

--- Cognito User Pools Authentication Lab/Cognito.cs
+++ Cognito User Pools Authentication Lab/Cognito.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright <first-edit-year> Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: MIT-0
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
